8bf2ec91296a5874b8916e4b1b05f179d2609f60
eeapbh <gitlab-test@example.com>
2023-07-14 17:47:23 +0900

    update

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import RedirectButton from "./components/RedirectButton";
 function App() {
   return (
     <div className="App">
-      <RedirectButton url="http://argo-cd.woobuntu.shop" />
-      <RedirectButton url="http://prometheus.woobuntu.shop" />
-      <RedirectButton url="http://grafana.woobuntu.shop" />
+      <RedirectButton url="http://argo-cd.woobuntu.shop" btnName="argocd" />
+      <RedirectButton
+        url="http://prometheus.woobuntu.shop"
+        btnName="prometheus"
+      />
+      <RedirectButton url="http://grafana.woobuntu.shop" btnName="grafana" />
+      <a href="http://argo-cd.woobuntu.shop">argocd</a>
     </div>
   );
 }
--- src/components/RedirectButton.js
+++ src/components/RedirectButton.js
@@ -1,11 +1,16 @@
 import React from "react";
+import styles from "./RedirectButton.module.css";
 
-const RedirectButton = ({ url }) => {
+const RedirectButton = ({ url, btnName }) => {
   const handleClick = () => {
     window.location.href = url;
   };
 
-  return <button onClick={handleClick}>Go to {url}</button>;
+  return (
+    <button className={styles.redirectBtn} onClick={handleClick}>
+      Go to {btnName}
+    </button>
+  );
 };
 
 export default RedirectButton;
